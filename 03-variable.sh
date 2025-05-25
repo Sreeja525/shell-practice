28709d20ca022625f39649895f2057d2ae299fcc
Vijay Krishna <chiduralasreeja@gmail.com>
2025-05-25 21:34:12 +0530

    variable script

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+echo "$person1: Hi Vijay, How are you?"
+echo "$person2: Hi sreeja,fine"
+echo "$person1: ok"
+echo "$person2: how are you?"
+echo "$person1: great to hear"
