7392c8e84b0e436bb2c04e7c308b3e0619e20a7a
Vijay Krishna <chiduralasreeja@gmail.com>
2025-06-04 16:12:27 +0530

    shell practice

---
+++ arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("ready" "thandel" "pushpa" "Hit3")
+
+echo "first movie name is: ${MOVIES[0]}"
+echo "second movie name is : ${MOVIES[1]}"
+
+echo "all movies in our array are: ${MOVIES[@]}"
