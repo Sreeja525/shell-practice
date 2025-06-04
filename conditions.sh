77b9b058884b40a35fa7f2a958d73d006ac8deb7
Vijay Krishna <chiduralasreeja@gmail.com>
2025-06-04 16:34:20 +0530

    shell practice

---
+++ conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NUMBER=$1
+
+
+#-gt greater than
+#-lt less than
+#-eq equal
+#-ne not equal
+
+if [ $NUMBER -lt 10 ] 
+then 
+    echo " given number is less than 10"
+else
+    echo "given number is greater than 10"
+fi
