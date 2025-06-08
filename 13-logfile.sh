e905ea553895e1cdaafb7820df6681b52cb2a78b
chiduralasreeja@gmail.com <chiduralasreeja@gmail.com>
2025-06-08 21:29:54 +0530

    sheel

--- 13-logfile.sh
+++ 13-logfile.sh
@@ -8,9 +8,9 @@ N="\e[0m"
 
 LOGS_FOLDER=/var/log/shellscript-logs
 SCRIPT_NAME= $(echo $0 | cut -d "." -f1)
-LOG_FILE="$(LOGS_FOLDER)/$SCRIPT_NAME.log"
-echo "creating $(LOGS_FOLDER)"
-sudo mkdir -p $(LOGS_FOLDER)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+echo "creating $LOGS_FOLDER"
+sudo mkdir -p $LOGS_FOLDER
 
 echo "script started executed at : $(date)" | tee -a  $LOG_FILE 
 if [ $USERID -ne 0 ]
