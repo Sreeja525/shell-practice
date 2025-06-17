44a278a5f48c32d008e3c06eea799ab8b1435960
chiduralasreeja@gmail.com <chiduralasreeja@gmail.com>
2025-06-17 22:00:48 +0530

    shell practice

--- 14-loops.sh
+++ 14-loops.sh
@@ -13,35 +13,37 @@ echo "creating $LOGS_FOLDER"
 PACKAGE=("mysql" "nginx" "Python3" "httpd")
 
 
-mkdir -p $LOGS_FOLDER #It creates parent directories as needed (e.g., if /var/log/myapp/logs doesn’t exist, it will create the full path).
+mkdir -p $LOGS_FOLDER # -p --> It creates parent directories as needed (e.g., if /var/log/myapp/logs doesn’t exist, it will create the full path).
 echo "script started executed at : $(date)" | tee -a  $LOG_FILE 
 
 if [ $USERID -ne 0 ]
 then 
-    echo -e "$R ERROR: Please run with root access $N"
-    exit 1
+    echo -e "$R ERROR: Please run with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else    
-    echo -e "$G you are running with root access $N"
+    echo -e "$G you are running with root access $N" | tee -a $LOG_FILE
 fi
 
 VALIDATE(){
     if [ $1 -eq 0 ]
     then    
-        echo -e "$G you have successfuly installed $2 $N "
+        echo -e "$G you have successfuly installed $2 $N " | tee -a $LOG_FILE
     else
-        echo  -e " $R installing $2 is failure $N"
+        echo  -e " $R installing $2 is failure $N" | tee -a $LOG_FILE
+        exit 1 #give other than 0 upto 127
     fi
 }
 
-for  PACKAGE in ${PACKAGE[@]}
+#for  PACKAGE in ${PACKAGE[@]}
+for package in $@ #$@ Represents all arguments passed to the script as separate words
 do
     dnf list installed $PACKAGE &>> $LOG_FILE
     if [ $? -ne 0 ]
     then 
-        echo -e " $Y $PACKAGE is not installed.. going to install it $N"
+        echo -e " $Y $PACKAGE is not installed.. going to install it $N" | tee -a $LOG_FILE
         dnf install $PACKAGE -y
         VALIDATE $? "$PACKAGE"
     else
-        echo -e " $G $PACKAGE is already installed $N"
+        echo -e " $G $PACKAGE is already installed $N" | tee -a $LOG_FILE
     fi
 done
